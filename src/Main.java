1192123883a0badb88df19ffa7055db791611dd8
cellosign <cellosign@gmail.com>
2018-10-31 08:38:38 +0200

    add

--- src/Main.java
+++ src/Main.java
@@ -9,24 +9,18 @@ public class Main {
         while (wantToContinue == true) {
             System.out.println("Choose an action: 1 - add item, 2-delete item, 3-show list, 4-exit");
             String choice = sc.nextLine();
+
             if (choice.equals("1")) {
-                System.out.println("Enter name");
-                String name = sc.nextLine();
-                toDoList.add(name);
-                //add(toDoList,name);
+                addProduct(toDoList);
                 wantToContinue = true;
             }
             if (choice.equals("2")) {
-                System.out.println("Enter name");
-                String name = sc.nextLine();
-                System.out.println("The index of this item is: " + toDoList.lastIndexOf(name));
-                toDoList.remove(toDoList.indexOf(name)); //indeks nepraviljnij
+                removeProduct(toDoList);
                 wantToContinue = true;
                 //remove(name);
             }
             if (choice.equals("3")) {
-                System.out.println("Here is your to do list");
-                System.out.println(toDoList);//vivodit kakoj-to [name] vmesto spiska
+                showProducts(toDoList);
                 wantToContinue = true;
                 //showList();
             }
@@ -36,6 +30,27 @@ public class Main {
             }
         }
         sc.close();
+    }
+
+    private static void showProducts(ArrayList<String> toDoList) {
+        System.out.println("Here is your products' list");
+        System.out.println(toDoList);//vivodit kakoj-to [name] vmesto spiska
+    }
+
+    private static void removeProduct(ArrayList<String> toDoList) {
+        System.out.println("Enter name");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("The index of this item is: " + toDoList.lastIndexOf(name));
+        toDoList.remove(toDoList.indexOf(name)); //indeks nepraviljnij
+    }
+
+    private static void addProduct(ArrayList<String> toDoList) {
+        System.out.println("Enter name");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        toDoList.add(name);
+        //add(toDoList,name);
     }
 
 //    public static void add(String name) {
